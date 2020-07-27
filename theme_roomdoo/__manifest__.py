c31d72a04010ce4760c3ed669096961bcd050a26
Manuel Alejandro Núñez Liz <lordchucu@gmail.com>
2020-07-27 09:23:37 +0200

    [ADD] Odoo theme v 0.1

---
+++ theme_roomdoo/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Roomdoo Theme',
+    'description': 'Roomdoo Theme',
+    'category': 'Theme/Services',
+    'summary': 'Roomdoo Theme',
+    'sequence': 120,
+    'version': '1.1',
+    'depends': ['theme_common', ],
+    'data': [
+        'views/assets.xml',
+        'views/image_content.xml',
+        'views/image_library.xml',
+        'views/snippets_options.xml',
+        'views/snippets.xml',
+    ],
+    'images': [],
+    'license': 'MIT',
+    'live_test_url': 'https://www.zeleiro.gal',
+}
